4b2bf749eff041547844812400c76cd6f4b1669b
ridhimak29 <90280365+ridhimak29@users.noreply.github.com>
2022-01-09 20:14:26 -0500

    Initial commit

---
+++ src/main/java/practice/tacocloudridhima/TacoCloudRidhimaApplication.java
@@ -0,0 +1,13 @@
+package practice.tacocloudridhima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudRidhimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudRidhimaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practice/tacocloudridhima/TacoCloudRidhimaApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.tacocloudridhima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudRidhimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
